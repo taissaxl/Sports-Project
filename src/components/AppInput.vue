8bbe53e13257ace338e6bfadab6de269046b6646
Taíssa <taissa.leal@estudante.ufjf.br>
2024-01-29 20:16:26 -0300

    Create app input

---
+++ src/components/AppInput.vue
@@ -0,0 +1,17 @@
+<template>
+    
+    <input type="text" :value="value" @keyup="$emit('input', $event.target.value)" class="form-control" placeholder="Type in your club">
+
+</template>
+
+<script>
+export default {
+    props: {
+        value: String
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
