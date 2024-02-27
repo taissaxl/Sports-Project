61f0bff27b3bad967542daa30fdde5e8fd129e4c
Taíssa <taissa.leal@estudante.ufjf.br>
2024-02-27 09:06:28 -0300

    Add template

--- src/components/AppSectionNews.vue
+++ src/components/AppSectionNews.vue
@@ -8,9 +8,12 @@
                 :key="broadcast.id"
                 :img-name="broadcast.img"
                 :img-info="broadcast.imgInfo"
-                :news-title="broadcast.title"
                 :news-date="broadcast.date"
             >
+
+                <template #title>
+                    <h2>{{ broadcast.title }}</h2>
+                </template>
 
                 <p>{{ broadcast.content | truncate(200) }}</p>
 
@@ -23,6 +26,7 @@
 
 <script>
 import AppSectionIndividualNews from './AppSectionIndividualNews.vue';
+import { mapGetters } from 'vuex'
 
 export default {
     components: {
@@ -30,32 +34,13 @@ export default {
     },
     data() {
         return {
-            news: []
+  
         }
     },
-    created() {
-        this.news = [{
-            id: 1,
-            title: 'New training season has begun',
-            content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur molestias quaerat, dolores ullam vero? Veritatis accusantium voluptatibus magnam soluta nemo, voluptate similique illo ipsam vero. Quis asperiores quisquam culpa. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, similique in quaerat dolores ut consectetur tempora nemo eius amet exercitationem nihil, soluta nesciunt possimus molestias dicta! At quam asperiores ab?',
-            date: '2024-01-01',
-            img: 'news1.jpg',
-            imgInfo: 'First news'
-        }, {
-            id: 2,
-            title: 'Last Wednesdays game ends on a tie',
-            content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur molestias quaerat, dolores ullam vero? Veritatis accusantium voluptatibus magnam soluta nemo, voluptate similique illo ipsam vero. Quis asperiores quisquam culpa. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, similique in quaerat dolores ut consectetur tempora nemo eius amet exercitationem nihil, soluta nesciunt possimus molestias dicta! At quam asperiores ab?',
-            date: '2024-01-07',
-            img: 'news2.jpg',
-            imgInfo: 'Second news'
-        }, {
-            id: 3,
-            title: 'New stadium opening this month',
-            content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus pariatur molestias quaerat, dolores ullam vero? Veritatis accusantium voluptatibus magnam soluta nemo, voluptate similique illo ipsam vero. Quis asperiores quisquam culpa. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, similique in quaerat dolores ut consectetur tempora nemo eius amet exercitationem nihil, soluta nesciunt possimus molestias dicta! At quam asperiores ab?',
-            date: '2024-01-20',
-            img: 'news3.jpg',
-            imgInfo: 'Third news'    
-        }]
+    computed: {
+        ...mapGetters({
+            news: 'getNews'
+        })
     }
 }
 </script>
