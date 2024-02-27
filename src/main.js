452642666d637dd1734f7d7f6fd689e65d86580d
Taíssa <taissa.leal@estudante.ufjf.br>
2024-02-27 09:04:21 -0300

    Add store and router

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Vue2Filters from 'vue2-filters'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import store from './store/store'
+import router from './router/routes'
 import App from './App.vue'
 
 Vue.use(Vue2Filters)
@@ -10,4 +12,6 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  store,
+  router
 }).$mount('#app')
