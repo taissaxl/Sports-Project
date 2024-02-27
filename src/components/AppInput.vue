0d586704604d29bdd10ec79d454af32b279edbbd
Taíssa <taissa.leal@estudante.ufjf.br>
2024-02-27 09:05:19 -0300

    Add mapGetters and mapActions

--- src/components/AppInput.vue
+++ src/components/AppInput.vue
@@ -1,14 +1,17 @@
 <template>
     
-    <input type="text" :value="value" @keyup="$emit('input', $event.target.value)" class="form-control" placeholder="Type in your club">
+    <input type="text" :value="clubName" @keyup="updateClubName($event.target.value)" class="form-control" placeholder="Type in your club">
 
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
+
 export default {
-    props: {
-        value: String
-    }
+    computed: mapGetters({
+        clubName: 'getClubName'
+    }),
+    methods: mapActions(['updateClubName'])
 }
 </script>
 
